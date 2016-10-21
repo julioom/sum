7cf342f8b538c333661cde5c393e968c2968aff2
Your Name <you@example.com>
2016-10-21 09:25:23 +0100

    Primer commit

---
+++ test/sum/TestSum.java
@@ -0,0 +1,19 @@
+package sum;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSum {
+
+	@Test
+	public void testSum2Num2() {
+		assertEquals(4, sum(1,3));
+	}
+
+	
+	
+	int sum(int a, int b) {
+		return 4;
+	}
+}
