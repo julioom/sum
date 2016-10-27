3072e60329fd18a771febf4a4dab0ce7d88b5005
JoinHearthstone <joinhearthstone@outlook.es>
2016-10-27 17:00:35 +0200

    Final

--- test/sum/TestSum.java
+++ test/sum/TestSum.java
@@ -11,9 +11,21 @@ public class TestSum {
 		assertEquals(4, sum(1,3));
 	}
 
+
+	@Test
+	public void testSumNumWithZero() {
+		assertEquals(2, sum(2,0));
+		assertEquals(2, sum(0,2));
+	}
+
 	
+	@Test
+	public void testSumWithNeg() {
+		assertEquals(6, sum(8, -2));
+		assertEquals(-7, sum(-4, -3));
+	}
 	
 	int sum(int a, int b) {
-		return 4;
+		return Sum.sum(a, b);
 	}
 }
