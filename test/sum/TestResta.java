50828d632f47f128bcdc6bdcda31f727212f1c18
JoinHearthstone <joinhearthstone@outlook.es>
2016-10-27 17:04:50 +0200

    Primera prueba e impl trivial

---
+++ test/sum/TestResta.java
@@ -0,0 +1,19 @@
+package sum;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestResta {
+
+	@Test
+	public void restaDosNumeros() {
+		assertEquals(2, resta(7, 5));
+	}
+
+	private int resta(int i, int j) {
+		
+		return 2;
+	}
+
+}
