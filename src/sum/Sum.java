81925957c958b606df4b4e75a4a2eac9a70ef394
julioom96 <julioomar4c@gmail.com>
2018-01-18 09:57:04 +0100

    multiplicacion

--- src/sum/Sum.java
+++ src/sum/Sum.java
@@ -4,16 +4,16 @@ public class Sum {
 
 
 	public static int sum(int a, int b) {
-		return a+b;
+		return a*b;
 	}
 
 
 	public static int sum(int i, int j, int k) {
-		return sum(i, j)+k;
+		return sum(i, j)*k;
 	}
 	
 	public static int sum_untested(int a, int b) {
-		return a+b;
+		return a*b;
 	}
 	
 
